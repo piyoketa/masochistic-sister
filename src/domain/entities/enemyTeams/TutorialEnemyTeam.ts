6d7a4ea98f0bbe3ce94a724cc5a2921a2515677d
piyoketa <piyoketeru@gmail.com>
2025-12-31 00:03:39 +0900

    fix: Update TutorialEnemyTeam to use OrcLancerEnemy and refine initial actions

--- src/domain/entities/enemyTeams/TutorialEnemyTeam.ts
+++ src/domain/entities/enemyTeams/TutorialEnemyTeam.ts
@@ -1,13 +1,11 @@
 import { EnemyTeam } from '../EnemyTeam'
-import { OrcEnemy } from '../enemies/OrcEnemy'
+import { OrcLancerEnemy } from '../enemies/OrcLancerEnemy'
 import { SlugEnemy } from '../enemies/SlugEnemy'
 import { SnailEnemy } from '../enemies/SnailEnemy'
 import { FlurryAction } from '../actions/FlurryAction'
 import { DefaultEnemyActionQueue } from '../enemy/actionQueues'
 import { TackleAction } from '../actions/TackleAction'
 import { AcidSpitAction } from '../actions/AcidSpitAction'
-import { Damages } from '../Damages'
-import { BuildUpAction } from '../actions/BuildUpAction'
 import { createMembersWithLevels } from './bonusLevelUtils'
 
 export interface TutorialEnemyTeamOptions {
@@ -17,22 +15,18 @@ export interface TutorialEnemyTeamOptions {
 
 /**
  * チュートリアル用の固定敵チーム。
- * - オーク: 初手フルーリー(10×2)を確定。
+ * - オークランサー: 初手フルーリー(10×2)を確定し、以降はデフォルト行動ローテーション。
  * - なめくじ: HP30 / 初手殴打(20)。
  * - かたつむり: 初手「溶かす」(AcidSpit)。
  */
 export class TutorialEnemyTeam extends EnemyTeam {
   constructor(options?: TutorialEnemyTeamOptions) {
-    const flurry = new FlurryAction().cloneWithDamages(
-      new Damages({ baseAmount: 10, baseCount: 2, type: 'multi', cardId: 'flurry' }),
-    )
     const rng = options?.rng ?? Math.random
     const members = createMembersWithLevels(
       [
         (level) =>
-          new OrcEnemy({
+          new OrcLancerEnemy({
             level,
-            actions: [flurry, new TackleAction(), new BuildUpAction()],
             actionQueueFactory: () => new DefaultEnemyActionQueue({ initialActionType: FlurryAction }),
           }),
         (level) =>
